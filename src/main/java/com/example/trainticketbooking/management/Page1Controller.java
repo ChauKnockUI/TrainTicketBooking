7611bc6ee00d3189dd02e942f1d19d5ae04c747e
ChauKnockUI <hohaichau963@gmail.com>
2024-12-06 11:41:13 +0700

    ADMIN lay GaDi GaDen

--- src/main/java/com/example/trainticketbooking/management/Page1Controller.java
+++ src/main/java/com/example/trainticketbooking/management/Page1Controller.java
@@ -1,7 +1,9 @@
 package com.example.trainticketbooking.management;
 
 import com.example.trainticketbooking.TrainListController;
+import comp.Rmi.model.Station;
 import comp.Rmi.model.Tuyen;
+import comp.Rmi.rmi.StationService;
 import comp.Rmi.rmi.TuyenService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -16,7 +18,17 @@ public class Page1Controller {
     private ComboBox<String> routeIdComboBox;
 
     @FXML
+    private ComboBox<String> departureStationComboBox;
+
+    @FXML
+    private ComboBox<String> arrivalStationComboBox;
+
+    @FXML
     public void initialize() {
+        getTuyen();
+        getGaDiVaGaDen();
+    }
+    public void getTuyen(){
         try {
             // Kết nối tới RMI Registry
             Registry registry = LocateRegistry.getRegistry(TrainListController.GlobalConfig.serverIP, 1099);
@@ -28,6 +40,25 @@ public class Page1Controller {
             // Thêm các tuyến vào ComboBox
             for (Tuyen tuyen : tuyenList) {
                 routeIdComboBox.getItems().add(tuyen.getTen()); // Sử dụng phương thức getTuyenName() hoặc tên khác
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void getGaDiVaGaDen(){
+        try {
+            // Kết nối tới RMI Registry
+            Registry registry = LocateRegistry.getRegistry(TrainListController.GlobalConfig.serverIP, 1099);
+            StationService stationService = (StationService) registry.lookup("StationService");
+
+            // Gọi phương thức getAllStations() để lấy danh sách các ga
+            List<Station> stations = stationService.getAllStations();
+
+            // Thêm danh sách ga vào hai ComboBox
+            for (Station station : stations) {
+                String stationName = station.getTen(); // Giả sử Station có phương thức getStationName()
+                departureStationComboBox.getItems().add(stationName);
+                arrivalStationComboBox.getItems().add(stationName);
             }
         } catch (Exception e) {
             e.printStackTrace();
